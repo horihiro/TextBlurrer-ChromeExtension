113758ddd1c6f21416041091834de0df8e3a3713
horihiro <hori.kawa.shima.tool+github@gmail.com>
2023-08-22 11:38:33 +0900

    exclude `contentEditable` element

--- content/js/main.js
+++ content/js/main.js
@@ -1,6 +1,10 @@
 (async () => {
   const w = window;
-  const exElmList = ['title', 'script', 'noscript', 'style', 'meta', 'link', 'head', 'textarea'];
+  const exElmList = ['html', 'title', 'script', 'noscript', 'style', 'meta', 'link', 'head', 'textarea'];
+  const getStateOfContentEditable = (element) => {
+    if (element.contentEditable !== 'inherit') return element.contentEditable;
+    return element.parentNode ? getStateOfContentEditable(element.parentNode) : ''; 
+  };
   const blur = (keywords) => {
     if (w.__observer) return;
     const blurKeywords = (keywords) => {
@@ -11,12 +15,12 @@
           return !exElmList.includes(n.nodeName.toLowerCase())
             && Array.prototype.filter.call(n.childNodes, (c) => {
               return c.nodeName === '#text' && c.data.includes(keyword);
-            }).length > 0;
+            }).length > 0
+            && getStateOfContentEditable(n) !== 'true';
         }).forEach((n) => {
           const re = new RegExp(keyword.replace(/([()?$])/g, "\\$1"), 'g');
           try {
-            !!n.className && !n.className.includes('blurred') && (n.innerHTML = n.innerHTML.replace(re, `<span class="blurred">${keyword}</span>`));
-            !n.className && (n.innerHTML = n.innerHTML.replace(re, `<span class="blurred">${keyword}</span>`));
+            (!n.className || !n.className.includes('blurred')) && (n.innerHTML = n.innerHTML.replace(re, `<span class="blurred">${keyword}</span>`));
           } catch (e) {
             n.innerHTML = n.innerHTML.replace(re, ``);
           }
