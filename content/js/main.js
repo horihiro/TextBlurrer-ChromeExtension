830f927091164062268de88ac887bc181ba2d069
horihiro <hori.kawa.shima.tool+github@gmail.com>
2023-10-21 12:53:39 +0900

    fix `title`attribuet issue

--- content/js/main.js
+++ content/js/main.js
@@ -208,7 +208,13 @@
         ) {
           n.classList.add(blurredClassName);
           n.classList.add(keepClassName);
-          options?.showValue && n.setAttribute('title', o.keyword);
+          if (options?.showValue) {
+            const originalTitle = n.getAttribute('title');
+            if (originalTitle) {
+              n.setAttribute('data-tb-original-title', originalTitle);
+            }
+            n.setAttribute('title', o.keyword);
+          }
           if (size > 5) n.style.filter += ` blur(${size}px)`;
           return;
         }
@@ -443,6 +449,14 @@
     const now = Date.now();
     m.forEach((n) => {
       if (n.classList.contains(blurredClassName) && n.classList.contains(keepClassName)) {
+        // restore title
+        const originalTitle = n.getAttribute('data-tb-original-title');
+        if (originalTitle) {
+          n.setAttribute('title', originalTitle);
+          n.removeAttribute('data-tb-original-title');
+        }
+        else n.removeAttribute('title');
+
         // restore class
         n.classList.remove(blurredClassName);
         n.classList.remove(keepClassName);
