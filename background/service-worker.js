9cec9ab759662f72813a5ddcb767944d8c8cf49c
horihiro <hori.kawa.shima.tool+github@gmail.com>
2024-04-30 08:58:47 +0900

    Replace `&nbsp` to half-width white space on storing

--- background/service-worker.js
+++ background/service-worker.js
@@ -14,7 +14,7 @@ chrome.contextMenus.onClicked.addListener(async(info, tab) => {
     (keywords.length === 1 && keywords[0] === '') && keywords.pop();
     const addingKeyword = (await chrome.storage.local.get(['mode']))?.mode === 'regexp' ? escapeRegExp(info.selectionText) : info.selectionText;
     !keywords.includes(addingKeyword) && keywords.push(addingKeyword);
-    await chrome.storage.local.set({ 'keywords': keywords.join('\n') });
+    await chrome.storage.local.set({ 'keywords': keywords.join('\n').replace(/\u00a0/g, ' ') });
     chrome.runtime.sendMessage({ method: 'reload' });
   }
 });
